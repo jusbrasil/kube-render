af71a29576b84e65141271d43058ed87a71d6887
Gabriel Jordao <gabrielpjordao@gmail.com>
2017-04-15 04:05:46 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    requirements = f.readlines()
+__version__ = '0.1.1'
 
 setup(
-    name='kube-render',
-    version='0.1',
+    name='kuberender',
+    version=__version__,
     py_modules=['kuberender'],
-    install_requires=requirements,
+    install_requires=[
+      'Jinja2==2.9.6',
+      'click==6.7',
+      'PyYAML==3.12',
+    ],
     entry_points='''
         [console_scripts]
         kube-render=kuberender:run
