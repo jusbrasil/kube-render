d75479ceadb9a26549f1cad2e931915f33dfaf8c
Gabriel Jordao <gabrielpjordao@gmail.com>
2017-05-07 13:47:37 -0300

    Separate render logic from run entrypoint

--- kuberender.py
+++ kuberender.py
@@ -69,16 +69,7 @@ def update_templates(template_url, dump_dir):
     repo = create_repo_from_pip_url(pip_url=template_url, repo_dir=dump_dir)
     repo.update_repo()
 
-
-@click.command()
-@click.option('--verbose', '-v', default=False, is_flag=True, help='Whether it should print generated files or not')
-@click.option('--context', '-c', 'context_files', help="Yaml file path to be loaded into context. Supports merging.", multiple=True)
-@click.option('--set', '-s', 'overriden_vars', help="Vars that override context files. Format: key=value", multiple=True)
-@click.option('--template-dir', '-t', default='templates', help='Folder holding templates that should be rendered')
-@click.option('--template-url', '-u', default=None, help='URL to download templates from (writes on ~/.kube-render/templates). Accepts URLs on pip format')
-@click.option('--apply', '-A', 'should_apply', default=False, is_flag=True, help="Apply rendered files using `kubectl apply`")
-@click.option('--working-dir', '-w', default='.', help="Directory where jinja will find all files")
-def run(verbose, template_dir, should_apply, context_files, overriden_vars, template_url, working_dir):
+def render(verbose, template_dir, should_apply, context_files, overriden_vars, template_url, working_dir):
     change_working_dir = partial(os.path.join, working_dir)
     context_data = map(load_yaml_file, map(change_working_dir, context_files))
 
@@ -104,3 +95,15 @@ def run(verbose, template_dir, should_apply, context_files, overriden_vars, temp
     if should_apply:
         map(call_kubectl_apply, rendered_templates)
     return rendered_templates
+
+
+@click.command()
+@click.option('--verbose', '-v', default=False, is_flag=True, help='Whether it should print generated files or not')
+@click.option('--context', '-c', 'context_files', help="Yaml file path to be loaded into context. Supports merging.", multiple=True)
+@click.option('--set', '-s', 'overriden_vars', help="Vars that override context files. Format: key=value", multiple=True)
+@click.option('--template-dir', '-t', default='templates', help='Folder holding templates that should be rendered')
+@click.option('--template-url', '-u', default=None, help='URL to download templates from (writes on ~/.kube-render/templates). Accepts URLs on pip format')
+@click.option('--apply', '-A', 'should_apply', default=False, is_flag=True, help="Apply rendered files using `kubectl apply`")
+@click.option('--working-dir', '-w', default='.', help="Directory where jinja will find all files")
+def run(verbose, template_dir, should_apply, context_files, overriden_vars, template_url, working_dir):
+    return render(verbose, template_dir, should_apply, context_files, overriden_vars, template_url, working_dir)
